5cd8be040623da87b6ee810b2415e5a2d366e4c1
кирилл смушков <kirillsmuskov@gmail.com>
2024-02-04 19:14:13 +0700

    Interface

--- Data/Views/Vxod.xaml.cs
+++ Data/Views/Vxod.xaml.cs
@@ -10,28 +10,23 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ColorManager.Data.Views
 {
-    public partial class MainWindow : Window
+    /// <summary>
+    /// Логика взаимодействия для Vxod.xaml
+    /// </summary>
+    public partial class Vxod : Window
     {
-        public MainWindow()
+        public Vxod()
         {
             InitializeComponent();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Registr form = new Registr();
-            form.Show();
-            this.Hide();
-        }
-
-        private void Button_Click1(object sender, RoutedEventArgs e)
-        {
-            Vxod form = new Vxod();
+            MainWindow form = new MainWindow();
             form.Show();
             this.Hide();
         }
