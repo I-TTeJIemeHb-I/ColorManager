c8fb7077b48980ddb5c59f491fec16fda44abbd8
Данила Харитонов <danila.kharitonov1999@gmail.com>
2024-02-05 17:58:12 +0700

    Доработка наигации в приложении

--- Data/Views/MainWindow.xaml.cs
+++ Data/Views/MainWindow.xaml.cs
@@ -1,18 +1,4 @@
-﻿using ColorManager.DataBase;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace ColorManager.Data.Views
 {
@@ -21,36 +7,6 @@ namespace ColorManager.Data.Views
         public MainWindow()
         {
             InitializeComponent();
-
-            using (var db = new ApplicationContext())
-            {
-                var users = db.Users;
-
-                //var user = users.FirstOrDefault(u => u.Login == "1" && u.Password == "1");
-
-                //if (user != null)
-                //{
-                //    MessageBox.Show($"Имя пользователя: {user.Name}");
-                //}
-                //else
-                //{
-                //    MessageBox.Show($"Такого пользователя не существует");
-                //}
-            }
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Registr form = new Registr();
-            form.Show();
-            this.Hide();
-        }
-
-        private void Button_Click1(object sender, RoutedEventArgs e)
-        {
-            Vxod form = new Vxod();
-            form.Show();
-            this.Hide();
         }
     }
 }
