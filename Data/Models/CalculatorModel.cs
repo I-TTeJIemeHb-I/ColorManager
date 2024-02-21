183e1c7572681a6b4164f3c79e9a763664ad0684
kopilovroman228@gmail.com <kopilovroman228@gmail.com>
2024-02-21 09:58:33 +0700

    Create CalculatorModel

---
+++ Data/Models/CalculatorModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ColorManager.Data.Models
+{
+    public class CalculatorModel
+    {
+        public static string? NameProduct;
+        public static string? FanOfColor;
+        public static SolidColorBrush? Color;
+        public static bool? Availability;
+        public static string? ColorSpectrum;
+        public static string? Name_FanOfColor_Color;
+
+
+        public CalculatorModel(string name, string fanOfColor,SolidColorBrush color)
+        {
+            NameProduct = name;
+            FanOfColor = fanOfColor;
+            Color = color;
+
+            Name_FanOfColor_Color = $"{NameProduct}, {FanOfColor}, {Color}";
+        }
+       
+    }
+}
