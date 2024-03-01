b76442f463553f4a708063a79fb2026de7a3108b
kopilovroman228@gmail.com <kopilovroman228@gmail.com>
2024-03-01 15:19:35 +0700

    HomePageViewModel

---
+++ Data/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ColorManager.Data.ViewModels
+{
+    public class HomePageViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+
+        private RelayCommand _goToColorStation;
+
+
+        public RelayCommand GoToColorStation
+        {
+            get
+            {
+                return _goToColorStation ??=
+                    new RelayCommand(obj =>
+                    {
+                        Page page = obj as Page;
+                        page.NavigationService.Navigate(new Views.ColorStantion());
+                    });
+            }
+        }
+    }
+}
